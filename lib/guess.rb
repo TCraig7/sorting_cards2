1be781a7bb6b48aab799924e9af54f2b4f7eaf02
Tara Craig <tara.craig7@gmail.com>
2018-06-30 10:40:52 -0600

    Add correct? method and test

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,10 +1,18 @@
 class Guess
 
-  attr_reader   :response
+  attr_reader   :response,
                 :card
 
   def initialize(response, card)
     @response = response
     @card = card
+  end
+
+  def correct?
+    if @response == "#{card.value} of #{card.suit}"
+      true
+    else
+      false
+    end
   end
 end
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -11,4 +11,19 @@ class GuessTest < Minitest::Test
 
     assert_instance_of Guess, guess
   end
+
+  def test_it_has_attributes
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+
+    assert_equal card, guess.card
+    assert_equal "10 of Hearts", guess.response
+  end
+
+  def test_response_is_correct
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+
+    assert_equal true, guess.correct?
+  end
 end
